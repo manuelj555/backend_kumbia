d6f7a2de998e1e5e20a82f61d2779e58efeb07a2
Manuel Aguirre <programador.manuel@gmail.com>
2012-02-16 09:06:01 -0430

    cambio en el filtro de auditorias para que coloque el formato de fecha de mysql

--- app/models/auditorias.php
+++ app/models/auditorias.php
@@ -1,26 +1,27 @@
 <?php
+
 /**
-* Backend - KumbiaPHP Backend
-* PHP version 5
-* LICENSE
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-* @package Modelos
-* @license http://www.gnu.org/licenses/agpl.txt GNU AFFERO GENERAL PUBLIC LICENSE version 3.
-* @author Manuel José Aguirre Garcia <programador.manuel@gmail.com>
-*/
+ * Backend - KumbiaPHP Backend
+ * PHP version 5
+ * LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Modelos
+ * @license http://www.gnu.org/licenses/agpl.txt GNU AFFERO GENERAL PUBLIC LICENSE version 3.
+ * @author Manuel José Aguirre Garcia <programador.manuel@gmail.com>
+ */
 class Auditorias extends ActiveRecord {
 
 //    public $debug = true;
@@ -37,7 +38,7 @@ class Auditorias extends ActiveRecord {
             $condiciones[] = "tabla_afectada LIKE '%{$filtro['tabla_afectada']}%'";
         }
         if (!empty($filtro['fecha_at'])) {
-
+            $filtro['fecha_at'] = date('Y-m-d', strtotime($filtro['fecha_at']));
             $condiciones[] = "fecha_at LIKE '%{$filtro['fecha_at']}%'";
         }
         if (!empty($filtro['accion_realizada'])) {
