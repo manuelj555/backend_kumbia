ed39a0dedc6f8a9b46848dfd5c3525fca51ca0fe
Manuel Aguirre <programador.manuel@gmail.com>
2012-02-21 19:55:01 -0430

    ahora te mantiene en la pagina donde estas editando los privilegios, es decir, no te manda a la pagina 1 al cambiar permisos

--- app/controllers/admin/privilegios_controller.php
+++ app/controllers/admin/privilegios_controller.php
@@ -1,26 +1,27 @@
 <?php
+
 /**
-* Backend - KumbiaPHP Backend
-* PHP version 5
-* LICENSE
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-* @package Controller
-* @license http://www.gnu.org/licenses/agpl.txt GNU AFFERO GENERAL PUBLIC LICENSE version 3.
-* @author Manuel José Aguirre Garcia <programador.manuel@gmail.com>
-*/
+ * Backend - KumbiaPHP Backend
+ * PHP version 5
+ * LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Controller
+ * @license http://www.gnu.org/licenses/agpl.txt GNU AFFERO GENERAL PUBLIC LICENSE version 3.
+ * @author Manuel José Aguirre Garcia <programador.manuel@gmail.com>
+ */
 class PrivilegiosController extends AdminController {
 
     public $model = 'roles_recursos';
@@ -35,7 +36,7 @@ class PrivilegiosController extends AdminController {
         }
     }
 
-    public function asignar_privilegios() {
+    public function asignar_privilegios($page = 1) {
         //por ahora este paso no es auditable :-s
         try {
             if (Input::hasPost('priv') || Input::hasPost('privilegios_pagina')) {
@@ -51,7 +52,7 @@ class PrivilegiosController extends AdminController {
         } catch (KumbiaException $e) {
             View::excepcion($e);
         }
-        return Router::redirect();
+        return Router::toAction("index/$page");
     }
 
 }
