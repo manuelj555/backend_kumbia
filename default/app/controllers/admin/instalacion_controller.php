5eb684b2cb584d6c19ad8074347c02e6737fd9cf
Manuel Aguirre <programador.manuel@gmail.com>
2012-04-14 23:42:08 -0430

    se agregó el controlador instalacion y se cambio en el routes
    se activó el routes para que apunte por defecto al controller instalacion
    este controlador será el encargado de llevar el proceso de instalación
    y configuracion del backend en el servidor...

---
+++ default/app/controllers/admin/instalacion_controller.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Description of instalacion_controller
+ *
+ * @author manuel
+ */
+
+class InstalacionController extends AppController {
+    
+    public function index(){
+        View::select(NULL);
+        try{
+            ob_start();
+            //var_dump(Load::model('usuarios')->find());
+            //$r = current(Db::factory()->find('usuarios'));
+            //var_dump($r['roles_id']);
+            var_dump(Db::factory()->find('usuarios'));
+            Flash::info(Db::factory()->last_sql_query());
+            Flash::info(Db::factory()->last_sql_query());
+            $usuarios = Load::model('usuarios');
+            $usuarios->find();
+            Flash::info($usuarios->db->last_sql_query());
+            
+        } catch (KumbiaException $e){
+            //si llegamos aqui es porque no se pudo conectar y no se ha configurado
+            //la conección
+            ob_clean();
+            Flash::info('Configurar COnexion a la Bd');
+        }
+
+        
+    }
+}
