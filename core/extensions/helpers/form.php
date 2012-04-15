f3e5434ea59590761a872ed9aedb65901616cc09
Manuel Aguirre <programador.manuel@gmail.com>
2012-04-14 22:33:27 -0430

    correciones al helper form, ya que se hicieron cambios
    y no habian quedado del todo bien, pero ahora trabaja correctamente...

--- core/extensions/helpers/form.php
+++ core/extensions/helpers/form.php
@@ -162,7 +162,7 @@ class Form
         }
 
         // Obtiene name, id y value (solo para autoload) para el campo y los carga en el scope
-        extract(self::_getFieldData($field, $value), EXTR_OVERWRITE);
+        extract(self::getFieldData($field, $value), EXTR_OVERWRITE);
 
         return "<input id=\"$id\" name=\"$name\" type=\"$type\" value=\"$value\" $attrs/>";
     }
@@ -366,7 +366,7 @@ class Form
         }
 
         // Obtiene name y id para el campo y los carga en el scope
-        extract(self::getFieldData($field, $checked === NULL), EXTR_OVERWRITE);
+        extract(self::getFieldData($field, $checked), EXTR_OVERWRITE);
 
         if ($checked || ($checked === NULL && $checkValue == $value)) {
             $checked = 'checked="checked"';
@@ -391,7 +391,7 @@ class Form
         }
 
         // Obtiene name y id para el campo y los carga en el scope
-        extract(self::getFieldData($field, $checked === NULL), EXTR_OVERWRITE);
+        extract(self::getFieldData($field, $checked), EXTR_OVERWRITE);
 
         if ($checked || ($checked === NULL && $radioValue == $value)) {
             $checked = 'checked="checked"';
