94e82a34d5934ea6bcad7b86e3c7ab88dade9214
Manuel Aguirre <programador.manuel@gmail.com>
2012-04-15 00:44:30 -0430

    agregando un draft de lo que será el primer paso del instalador
    Probando con un form el guardar la config de conexion a la bd.

--- default/app/controllers/admin/instalacion_controller.php
+++ default/app/controllers/admin/instalacion_controller.php
@@ -5,30 +5,38 @@
  *
  * @author manuel
  */
+class InstalacionController extends AppController
+{
 
-class InstalacionController extends AppController {
-    
-    public function index(){
-        View::select(NULL);
-        try{
-            ob_start();
-            //var_dump(Load::model('usuarios')->find());
-            //$r = current(Db::factory()->find('usuarios'));
-            //var_dump($r['roles_id']);
-            var_dump(Db::factory()->find('usuarios'));
-            Flash::info(Db::factory()->last_sql_query());
-            Flash::info(Db::factory()->last_sql_query());
-            $usuarios = Load::model('usuarios');
-            $usuarios->find();
-            Flash::info($usuarios->db->last_sql_query());
-            
-        } catch (KumbiaException $e){
-            //si llegamos aqui es porque no se pudo conectar y no se ha configurado
-            //la conección
-            ob_clean();
-            Flash::info('Configurar COnexion a la Bd');
+    public function index()
+    {
+//        View::select(NULL);
+//        try{
+//            ob_start();
+//            //var_dump(Load::model('usuarios')->find());
+//            //$r = current(Db::factory()->find('usuarios'));
+//            //var_dump($r['roles_id']);
+//            var_dump(Db::factory()->find('usuarios'));
+//            Flash::info(Db::factory()->last_sql_query());
+//            Flash::info(Db::factory()->last_sql_query());
+//            $usuarios = Load::model('usuarios');
+//            $usuarios->find();
+//            Flash::info($usuarios->db->last_sql_query());
+//
+//        } catch (KumbiaException $e){
+//            //si llegamos aqui es porque no se pudo conectar y no se ha configurado
+//            //la conección
+//            ob_clean();
+//            Flash::info('Configurar COnexion a la Bd');
+//        }
+        Config::read('databases');
+        $this->database = Config::get('databases.development');
+
+        if (Input::hasPost('database')) {
+            Config::set('databases.development', Input::post('database'));
+            MyConfig::save('databases');
+            Flash::info('se guardó');
         }
-
-        
     }
+
 }
