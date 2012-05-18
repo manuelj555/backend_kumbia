9a93ba0c9d71b9e30a49d447ede17865fda9697c
Manuel Aguirre <programador.manuel@gmail.com>
2012-05-18 14:41:25 -0430

    escapado el nombre del user en la bienvenida

--- default/app/libs/admin_controller.php
+++ default/app/libs/admin_controller.php
@@ -49,7 +49,7 @@ class AdminController extends Controller
     protected function _logueoValido($user, $pass, $encriptar = TRUE)
     {
         if (MyAuth::autenticar($user, $pass, $encriptar)) {
-            Flash::info('Bienvenido al Sistema <b>' . Auth::get('nombres') . '</b>');
+            Flash::info('Bienvenido al Sistema <b>' . h(Auth::get('nombres')) . '</b>');
             return $this->_tienePermiso();
         } else {
             Input::delete();
